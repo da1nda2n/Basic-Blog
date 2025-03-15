c03aa9117eee5ecbe12186d2e09469b6dd17d505
da1nda2n <da1nda2n@gmail.com>
2025-03-15 17:17:27 +0900

    swagger url추가 및 MYSQL 의존성 추가

--- src/main/java/com/example/blog/config/SwaggerConfig.java
+++ src/main/java/com/example/blog/config/SwaggerConfig.java
@@ -1,18 +1,27 @@
-package com.example.blog.post.config;
+package com.example.blog.config;
 
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
 
+    @Value("${swagger.server-url}")
+    private String serverUrl;
+
     @Bean
     public OpenAPI openAPI() {
+        Server server = new Server();
+        server.url(serverUrl);
+
         return new OpenAPI()
+                .addServersItem(server)
                 .components(new Components())
                 .info(apiInfo());
     }
