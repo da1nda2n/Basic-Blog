c3f4063f9e4c3ed0cc4324bbec7d6822f9fe13fd
da1nda2n <da1nda2n@gmail.com>
2025-03-22 02:25:04 +0900

    회원조회 userId 추가

--- src/main/java/com/example/blog/user/dto/res/UserGetResponseDto.java
+++ src/main/java/com/example/blog/user/dto/res/UserGetResponseDto.java
@@ -8,13 +8,15 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class UserGetResponseDto {
+    private Long userId;
     private String name;
     private String location;
     private String birth;
     private String phone;
 
     @Builder
-    public UserGetResponseDto(String name, String location, String birth, String phone) {
+    public UserGetResponseDto(Long userId, String name, String location, String birth, String phone) {
+        this.userId = userId;
         this.name = name;
         this.location = location;
         this.birth = birth;
@@ -23,6 +25,7 @@ public class UserGetResponseDto {
 
     public static UserGetResponseDto from(UserEntity user) {
         return UserGetResponseDto.builder()
+                .userId(user.getUserId())
                 .name(user.getName())
                 .location(user.getLocation())
                 .birth(user.getBirth())
